bb0eeced92f0a11c95f3688614ca5c1b9edddecd
FebriPutraPetir <fpp255@gmail.com>
2025-06-23 17:24:19 +0700

    Komit 001

--- info.php
+++ info.php
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 <?php
 echo 'open_basedir: ' . ini_get('open_basedir') . "<br>";
 session_save_path(__DIR__ . "/application/cache/sessions");
 session_start();
 $_SESSION['test'] = 'ok';
 echo 'Session path: ' . session_save_path();
+=======
+<?php
+echo 'open_basedir: ' . ini_get('open_basedir') . "<br>";
+session_save_path(__DIR__ . "/application/cache/sessions");
+session_start();
+$_SESSION['test'] = 'ok';
+echo 'Session path: ' . session_save_path();
+>>>>>>> 9cff4e3 (Komit 001)
